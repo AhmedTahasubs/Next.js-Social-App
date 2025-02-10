d66519fe92c30a4e4f2e3dfbba817f2c3212e985
AhmedTahasubs <ahmedtahaamuhammed@gmail.com>
2025-02-10 15:42:54 +0200

    another commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,8 +15,8 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-explicit-any': 'off', // Disable the rule
       '@typescript-eslint/no-unused-vars': 'off', // Disable the no-unused-vars rule
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
+      "react-hooks/rules-of-hooks": "off", // Checks rules of Hooks
+      "react-hooks/exhaustive-deps": "off" // Checks effect dependencies
     },
   },
 ];
